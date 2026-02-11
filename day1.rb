c9cbbd5d2ff5ed9fb70020acacb3026b62c39238
Hasan Tahir <hasan.tahir0303@gmail.com>
2026-02-11 07:54:28 +0500

    1st commit

---
+++ day1.rb
@@ -0,0 +1,62 @@
+# # a = "hello ruby!"
+# # puts a
+
+
+# # # Hours in a year. How many hours are in a year?
+
+# # day_hours = 24
+# # year_days = 365
+# # hoursinyear = day_hours * year_days
+
+# # puts "#{hoursinyear} hours in a year"
+
+# # # Minutes in a decade. How many minutes are in a decade?
+# # min_in_day = 24*60
+# # min_in_standard_year = min_in_day*365
+# # min_in_leapyear = min_in_day*366
+# # min_in_8standard_year =min_in_standard_year*8
+# # min_in_2leapyear = min_in_leapyear*2
+# # total_min_decade = min_in_2leapyear+min_in_8standard_year
+# # puts total_min_decade
+
+# #  Your age in seconds. How many seconds old are you? 
+
+# # age_in_days = 24*365
+# # leap_days= 6
+# # total_age_in_days = age_in_days+leap_days
+# # total_age_in_hours = total_age_in_days*24
+# # total_age_in_min = total_age_in_hours*60
+# # total_age_in_sec = total_age_in_min*60
+# # puts "Your total age in minutes:\n#{total_age_in_sec}"
+
+# # strings
+
+# # name = "ruby"
+# # puts "Hello #{name}"
+# # puts name.length
+# # puts name.upcase
+# # puts name.downcase
+# # puts name.split
+# # puts name.class 
+# #                         today task
+# # Create a simple calculator program
+# puts "Enter your 1st number: "
+# num1 = gets.chomp.to_i
+# puts "Enter your 2nd number: "
+# num2 = gets.chomp.to_i
+
+# puts "Sum of your numbers: #{num1+num2}"
+# puts "Subtraction of your numbers: #{num1-num2}"
+# puts "Multiplication of your numbers: #{num1*num2}"
+# puts  "devision = #{num1/num2}"
+# puts  "Modulo = #{num1%num2}"
+
+
+# # # String reversal and palindrome checker
+# # puts "Enter any word to see reverse or palindrome"
+# # word = gets.chomp
+# # reversal = word.reverse
+# # puts "Reversing result of #{word} : #{reversal}"
+# # puts "palindrome status of #{word} : #{reversal == word}"
+# # puts word[-3..-1]
+
---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,8 @@
+# String reversal and palindrome checker
+# puts "Enter any word to see reverse or palindrome"
+# word = gets.chomp
+# reversal = word.reverse
+# puts "Reversing result of #{word} : #{reversal}"
+# puts "palindrome status of #{word} : #{reversal == word}"
+# puts word[-3..-1]
+
