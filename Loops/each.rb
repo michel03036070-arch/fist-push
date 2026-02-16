c00e4fe4604fa8231aba85e39fd7a8b785dff77d
Hasan Tahir <hasan.tahir0303@gmail.com>
2026-02-16 13:24:24 +0500

    Each loop

---
+++ Loops/each.rb
@@ -0,0 +1,13 @@
+(1..10).each do |i|
+  puts "5 x #{i} = #{i*5}"
+end
+
+friends = ["mike", "Alice", "Bob"]
+friends.each do |lop|
+  puts lop
+end
+
+prices = [10, 25, 40]
+prices.each do |i|
+  puts "#{i} $"
+  end
