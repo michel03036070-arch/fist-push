550bce56fbb0daa05b4ebfefa5a011f8c6cdc837
Hasan Tahir <hasan.tahir0303@gmail.com>
2026-02-16 06:53:42 +0500

    While Loop

---
+++ Loops/whileloop.rb
@@ -0,0 +1,23 @@
+# count = 5
+
+# while count > 0
+#   puts "T-minus #{count}..."
+#   count -= 1
+# end
+
+# i = 1
+# total = 0
+# while i <= 10
+#   total += i
+#   i += 1
+# end
+# puts total
+
+puts "Enter Word: "
+word = gets.chomp.downcase
+while word != "ruby"
+  puts "you enter wrong word"
+  puts "Enter Word: "
+  word = gets.chomp.downcase
+end
+puts "Success"
